2eb4c4d537e0aaccdceee0ed1f60ef0740d77b3b
Sarwar <sarwar.cse4.bu@gmail.com>
2020-04-02 23:54:32 +0600

    Templates are linked with assets

--- CSEBU/urls.py
+++ CSEBU/urls.py
@@ -4,5 +4,11 @@ from django.urls import path
 from CSEBU import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('facultystuff', views.facultystuff, name='facultystuff'),
+    path('newsevent', views.newsevent, name='newsevent'),
+    path('research', views.research, name='research'),
+    path('campuslife', views.campuslife, name='campuslife'),
+    path('login', views.login, name='login')
 ]
--- CSEBU/views.py
+++ CSEBU/views.py
@@ -4,3 +4,24 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "homepage.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def facultystuff(request):
+    return render(request, "facultyandstuff.html")
+
+def newsevent(request):
+    return render(request, "newsandevents.html")
+
+def research(request):
+    return render(request, "research.html")
+
+def campuslife(request):
+    return render(request, "campuslife.html")
+
+
+def login(request):
+    return render(request, "login/login.html")
+
+
